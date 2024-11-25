2556961e06706d9fe54253ef3152d4eb485e9125
Dave Borghuis <dave@daveborghuis.nl>
2024-11-25 22:54:15 +0100

    update after first beta version

--- doorbell.py
+++ doorbell.py
@@ -7,31 +7,32 @@
 import time
 import signal
 import subprocess
-import os
-import uuid
 import requests
-import config
 from config import chaturl,telegram_token,telegram_chat
-from gpiozero import Button
+from gpiozero import Button, LED
 
 doorbell = Button(26,bounce_time=0.2)
+led = LED(14)
 
 def door_ring():
     print("Doorbel Ringing")
     send_notification(chaturl)
-    env = os.environ.copy()
-    subprocess.Popen(["aplay", "/home/zeno/deurbel/dingdong.wav"])
+    #env = os.environ.copy()
+    subprocess.Popen(["aplay", "/home/zeno/zip_pi_doorbell/dingdong.wav"])
     process = None
+    led.blink()
     if not process or not chaturl:
         print("Open url : ",chaturl)
         process = subprocess.Popen(["firefox", chaturl])
     else:
         print("Can't start video chat -- already started or missing chat id")
+    time.sleep(10)
+    led.on()
     time.sleep(100) #video time in seconds
     if process :
-        #process.terminate()
-        os.kill(process.pid, signal.SIGTERM)
+        process.terminate()
     print("....End")
+    led.off()
 
 def send_notification(msg):
     url = f'https://api.telegram.org/bot{telegram_token}/sendMessage'
@@ -52,5 +53,4 @@ def waitforbell():
         print("Setup Failed")
 
 if __name__ == "__main__":
-    print('Setup')
     waitforbell()
